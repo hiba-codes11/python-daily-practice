3fe72a3c23984f46289d0698b3cdfde70294f0b7
Hiba <hibabdul2005@gmail.com>
2026-08-12 16:44:50 +0530

    Day 1

---
+++ Day 1.py
@@ -0,0 +1,15 @@
+name = "Jasra"
+print(name, type(name))
+
+boolean = True
+print(boolean, type(boolean))
+
+age = 20
+print(age, type(age))
+
+score = 99.9
+print(score, type(score))
+
+developer = "Jasra"
+print(type(developer))
+
