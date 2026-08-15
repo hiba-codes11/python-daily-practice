dc798beb3a35c7ea78ff7bb8bda53dd7c6cf021c
Hiba <hibabdul2005@gmail.com>
2026-08-15 16:04:10 +0530

    Update bill

--- bill.py
+++ bill.py
@@ -20,4 +20,4 @@ final_bill = running_total / num_of_friends
 print('Bill per person:', final_bill)
 
 each_pays = round(final_bill, 2)
-print('Each person pays:', each_pays)
+print('💵 Each person pays:', each_pays)
