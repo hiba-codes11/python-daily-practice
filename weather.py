7b723dbf07c6bcde97deaa540e29cecc907f6934
Hiba <hibabdul2005@gmail.com>
2026-08-16 12:52:06 +0530

    Add weather

---
+++ weather.py
@@ -0,0 +1,21 @@
+distance_mi = 7
+is_raining = False
+has_bike = True
+has_car = False
+has_ride_share_app = False
+
+ # NOT flips the truth value; it turns a falsy value into True so the block can run.
+
+if not distance_mi:
+    print(False)
+
+elif distance_mi <= 1:
+    print(not is_raining)
+
+
+elif distance_mi <= 6:
+    print(has_bike and not is_raining)
+
+else: 
+    print(has_car or has_ride_share_app)
+
