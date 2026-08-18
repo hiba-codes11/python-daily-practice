4881da209a1d6d644db64b9ebe2e98378102cef1
Hiba <hibabdul2005@gmail.com>
2026-08-18 14:36:38 +0530

    Add discount

---
+++ discount.py
@@ -0,0 +1,31 @@
+def apply_discount(price, discount):
+
+    if not isinstance(price,(int, float)):
+        return 'The price should be a number'
+
+    if not isinstance(discount,(int, float)):
+        return 'The discount should be a number'
+
+    if price <= 0:
+        return 'The price should be greater than 0'
+
+    if discount < 0 or discount > 100:
+        return 'The discount should be between 0 and 100'
+    calculated_discount = price * (discount / 100)
+    
+    final_price = price - calculated_discount
+    return final_price
+    
+print(apply_discount(100, 20))
+print(apply_discount(200, 50))
+print(apply_discount(50, 0))
+print(apply_discount(0, 100))
+print(apply_discount(74.5, 20.0))
+
+# return and print are different:
+# return final_price
+# means:
+# "Give this value back to whoever called the function."
+# print(final_price)
+# means:
+# "Display this value on the screen."
